ee5054536ead67731445299be57011b41dbd5bf1
ninajhun <ninajhun@berkeley.edu>
2021-03-16 18:04:46 -0700

    completed ranson-case challenge

--- lfz-challenges/string-manipulation-2/src/ransom-case.js
+++ lfz-challenges/string-manipulation-2/src/ransom-case.js
@@ -1,1 +1,13 @@
 /* eslint-disable no-unused-vars */
+function ransomCase(string) {
+ let ransomWord = ''
+
+  for (let i = 0; i <= string.length - 1; i++) {
+    if (i % 2 === 0) {
+      ransomWord += string[i].toLowerCase()
+    } else {
+      ransomWord += string[i].toUpperCase()
+    }
+  }
+  return ransomWord
+}
