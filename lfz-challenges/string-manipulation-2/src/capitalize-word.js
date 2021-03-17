08d6c303eccada77d1fdc30285ce9afbf67093ed
ninajhun <ninajhun@berkeley.edu>
2021-03-16 17:49:59 -0700

    add capitalize-word challenge

--- lfz-challenges/string-manipulation-2/src/capitalize-word.js
+++ lfz-challenges/string-manipulation-2/src/capitalize-word.js
@@ -1,1 +1,8 @@
 /* eslint-disable no-unused-vars */
+function capitalizeWord(word){
+  if(word.toLowerCase() === "javascript"){
+    return "JavaScript"
+  }
+  return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()
+
+}
