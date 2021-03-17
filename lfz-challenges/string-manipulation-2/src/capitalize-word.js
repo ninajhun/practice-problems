196051718275e5d88ba5649a168e4e0a59be0d38
ninajhun <ninajhun@berkeley.edu>
2021-03-16 17:53:10 -0700

    completed capitalize-word challenge

--- lfz-challenges/string-manipulation-2/src/capitalize-word.js
+++ lfz-challenges/string-manipulation-2/src/capitalize-word.js
@@ -1,1 +1,7 @@
 /* eslint-disable no-unused-vars */
+function capitalizeWord(word){
+  if (word.toLowerCase() === 'javascript'){
+    return 'JavaScript'
+  }
+  return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()
+}
