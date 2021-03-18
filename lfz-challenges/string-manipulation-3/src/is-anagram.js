cc8f685c4fdbe3d53120fe45f778ae04fc88580b
ninajhun <ninajhun@berkeley.edu>
2021-03-17 20:20:18 -0700

    completed is-anagram & is-palindromic challenge

--- lfz-challenges/string-manipulation-3/src/is-anagram.js
+++ lfz-challenges/string-manipulation-3/src/is-anagram.js
@@ -1,1 +1,14 @@
 /* eslint-disable no-unused-vars */
+function isAnagram(firstString, secondString) {
+
+  const fArr = firstString.split("").sort()
+  const sArr = secondString.split("").sort()
+
+  return (fArr.join('').replaceAll(' ', '') === sArr.join('').replaceAll(' ', ''))
+
+}
+
+
+//remove spaces
+//change 2nd str to array
+//see if first string includes second string arr
--- lfz-challenges/string-manipulation-3/src/is-palindromic.js
+++ lfz-challenges/string-manipulation-3/src/is-palindromic.js
@@ -1,1 +1,7 @@
 /* eslint-disable no-unused-vars */
+function isPalindromic(string) {
+  string = string.replace(' ', '')
+  const reverseStr = string.split("").reverse().join("")
+  return reverseStr === string
+
+}
